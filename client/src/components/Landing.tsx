061f7240137f8ff44c3d88c5771254f5f0cf3af4
Prakhar <prakharb56@gmail.com>
2024-06-21 13:23:50 +0530

    helper changes

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -19,39 +19,42 @@ export default function Landing() {
     setTodos(response.data.todos);
   };
   return (
-    <div style={{ padding: "20px", textAlign: "center" }}>
-      <Button
-        variant="contained"
-        color="primary"
-        startIcon={<AddIcon />}
-        style={{ marginBottom: "20px" }}
-        onClick={() => {
-          navigate("/createTodo");
-        }}
-      >
-        Create Todo
-      </Button>
+    <>
       <Typography variant="caption">
         The Server is on a free tier so please wait a few seconds before it
         starts to server request, THANK YOU!
       </Typography>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          gap: "20px",
-          flexWrap: "wrap",
-        }}
-      >
-        {todos.map((todo) => (
-          <TodoCard
-            _id={todo._id}
-            title={todo.title}
-            description={todo.description}
-            date={todo.date}
-          />
-        ))}
+      <div style={{ padding: "20px", textAlign: "center" }}>
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<AddIcon />}
+          style={{ marginBottom: "20px" }}
+          onClick={() => {
+            navigate("/createTodo");
+          }}
+        >
+          Create Todo
+        </Button>
+
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "20px",
+            flexWrap: "wrap",
+          }}
+        >
+          {todos.map((todo) => (
+            <TodoCard
+              _id={todo._id}
+              title={todo.title}
+              description={todo.description}
+              date={todo.date}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
